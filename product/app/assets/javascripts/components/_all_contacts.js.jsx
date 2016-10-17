fd41f87d337b86cac5b14433490287d9a55c49fc
ChocolateAceCream <nuodi@hotmail.com>
2016-10-17 11:40:36 -0400

    finished delete function

--- product/app/assets/javascripts/components/_all_contacts.js.jsx
+++ product/app/assets/javascripts/components/_all_contacts.js.jsx
@@ -1,10 +1,17 @@
 var AllContacts = React.createClass ({
+
+    handleDelete(id){
+        console.log("delete contact clicked")
+        this.props.handleDelete(id);
+    },
+
     render() {
         var contacts = this.props.contacts.map((contact) => {
             return (
                 <div key={contact.id}>
                     <h3>{contact.name}</h3>
                     <p>{contact.phone}</p>
+                    <button onClick={this.handleDelete.bind(this, contact.id)}>Delete</button>
                 </div>
             )
         });
--- product/app/assets/javascripts/components/_body.js.jsx
+++ product/app/assets/javascripts/components/_body.js.jsx
@@ -1,25 +1,40 @@
 var Body = React.createClass({
-    getInitialState(){
-        return { contacts:[]}
-    },
+	getInitialState(){
+		return { contacts:[]}
+	},
 
-    componentDidMount(){
-        $.getJSON('/api/v1/contacts.json', (response) => {this.setState({ contacts: response}) });
-    },
+	componentDidMount(){
+		$.getJSON('/api/v1/contacts.json', (response) => {this.setState({ contacts: response}) });
+	},
 
-    handleSubmit(contact) {
-        console.log(contact);
-        var newState = this.state.contacts.concat(contact);
-        this.setState({ contacts: newState })
-    },
+	handleSubmit(contact) {
+		console.log(contact);
+		var newState = this.state.contacts.concat(contact);
+		this.setState({ contacts: newState })
+	},
+	handleDelete(id) {
+		$.ajax({
+			url: `/api/v1/contacts/${id}`,
+			type: 'DELETE',
+			success:() => { this.removecontactClient(id);
+			}
+		});
+	},
 
-    render() {
-        return (
-            <div>
-                <NewContact handleSubmit={this.handleSubmit} />
-                <AllContacts contacts = {this.state.contacts} />
-            </div>
-        )
-    }
+	removecontactClient(id) {
+		var newcontacts = this.state.contacts.filter((contact) => {
+			return contact.id != id;
+		});
+
+		this.setState({ contacts: newcontacts });
+	},
+	render() {
+		return (
+			<div>
+				<NewContact handleSubmit={this.handleSubmit} />
+				<AllContacts contacts = {this.state.contacts} handleDelete = {this.handleDelete}/>
+			</div>
+		)
+	}
 
 });
